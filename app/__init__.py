0b9d0b0841f0baf275f4204a3f9a9131052c7058
tfragner <fragner@bitter.at>
2018-05-20 13:11:27 +0200

    Einbindung von Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -11,5 +13,6 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
