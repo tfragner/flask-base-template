f7370bc4de7e3052071dc8d6169455d198ce2a9f
tfragner <fragner@bitter.at>
2018-05-20 13:22:18 +0200

    Tabelle User hinzugefügt

--- manage.py
+++ manage.py
@@ -1,15 +1,22 @@
 from flask_script import Manager
-from app import create_app
+from flask_migrate import MigrateCommand
+from app import create_app, db
+from app.models.user import User
 
 
 app = create_app()
 manager = Manager(app)
-
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
     app.run()
 
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
+    
 if __name__ == '__main__':
     manager.run()
