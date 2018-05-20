d7a405d89e29e60ba5cacb81abb8e5802dc8a02c
tfragner <fragner@bitter.at>
2018-05-20 13:09:02 +0200

    App und Management Script erstellt

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask_script import Manager
+from app import create_app
+
+
+app = create_app()
+manager = Manager(app)
+
+
+@manager.command
+def run():
+    app.run()
+
+
+if __name__ == '__main__':
+    manager.run()
