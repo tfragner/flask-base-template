ab9eaba48739c324d5b04a51241f7dc8bdc95a6d
tfragner <fragner@bitter.at>
2018-05-21 22:42:14 +0200

    redirect zur vorigen Seite beim Login

--- app/blog/__init__.py
+++ app/blog/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import redirect, url_for
+from flask import redirect, url_for, request
 from flask_login import current_user
 
 
@@ -9,7 +9,7 @@ bp = Blueprint('blog', __name__)
 @bp.before_request
 def retrict_bp():
     if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login', next=request.path))
 
 
 from app.blog import routes
